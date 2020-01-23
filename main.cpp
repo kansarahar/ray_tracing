68dd01c6bb523219eaa0f16341e207b92c76f556
Haresh Kansara <kansarahar@gmail.com>
2020-01-22 22:08:02 -0500

    removed a bunch of 'this->

--- main.cpp
+++ main.cpp
@@ -20,19 +20,21 @@ int main() {
 
 
     Sphere* sphere1 = new Sphere(vec3(0, 440, -70), 30);
-    Sphere* sphere2 = new Sphere(vec3(150, 440, -70), 30);;
+    Sphere* sphere2 = new Sphere(vec3(150, 440, -70), 30);
     Sphere* sphere3 = new Sphere(vec3(0, 440, -20), 30);
+    Sphere* sphere4 = new Sphere(vec3(150, 440, -70), 1000);
     
-    Plane* plane1 = new Plane(vec3(0,0,-100), vec3(0,0,1));//, vec3(150,150,150));
+    Plane* plane1 = new Plane(vec3(0,0,-100), vec3(0,0,1));
 
-    Cylinder* cylinder1 = new Cylinder(vec3(-120,420,-100), 20, 100);//, vec3(150,150,150));
+    Cylinder* cylinder1 = new Cylinder(vec3(-120,420,-100), 20, 100);
     
-    Cone* cone1 = new Cone(vec3(-100,370,-100), 30, 60);//, vec3(150,150,150));
+    Cone* cone1 = new Cone(vec3(-100,370,-100), 30, 60);
 
-    Triangle* tri1 = new Triangle(vec3(0,0,0), vec3(-100,0,100), vec3(100,0,100));//, vec3(250,0,0));
+    Triangle* tri1 = new Triangle(vec3(0,0,0), vec3(-100,0,100), vec3(100,0,100));
 
-    PointLight* plight1 = new PointLight(vec3(-50,350, 100), vec3(1,1,1), 1.3);
-    DirectionalLight *dlight1 = new DirectionalLight(vec3(-1,0,-1), vec3(1,1,1), 1.7);
+    PointLight* plight1 = new PointLight(vec3(-50,350, 100), vec3(1,1,1), 2.3);
+    PointLight* plight2 = new PointLight(vec3(150, 440, 400), vec3(1,1,1), 1.0);
+    DirectionalLight *dlight1 = new DirectionalLight(vec3(-1,0,-1), vec3(1,1,1), 0.7);
     DirectionalLight *dlight2 = new DirectionalLight(vec3(0,0,-1), vec3(1,1,1), 0.5);
     DirectionalLight *dlight3 = new DirectionalLight(vec3(0,1,-1), vec3(1,1,1), 0.7);
     AmbientLight *alight = new AmbientLight(vec3(1,1,1), 1);
@@ -41,13 +43,15 @@ int main() {
     scene.addSurface(sphere1);
     scene.addSurface(sphere2);
     scene.addSurface(sphere3);
+    scene.addSurface(sphere4);
     scene.addSurface(plane1);
     scene.addSurface(cylinder1);
     scene.addSurface(cone1);
     scene.addSurface(tri1);
 
     scene.addLight(plight1);
-    scene.addLight(dlight1);
+    scene.addLight(plight2);
+    // scene.addLight(dlight1);
     // scene.addLight(dlight2);
     // scene.addLight(dlight3);
     scene.addLight(alight);
@@ -55,6 +59,7 @@ int main() {
     sphere1->setTexture(vec3(255,80,80));
     sphere2->setTexture(vec3(80,255,80));
     sphere3->setTexture(vec3(80,80,255));
+    sphere4->setTexture(vec3(135, 206, 235));
     plane1->setTexture(vec3(150,150,150));
     cylinder1->setTexture(vec3(150,150,150));
     cone1->setTexture(vec3(150,150,150));
@@ -65,14 +70,14 @@ int main() {
     plane1->rotateSelf(vec3(1,0,0), 180);
     cone1->rotateSelf(vec3(0,1,0), -30);
     // cylinder1->rotateSelf(vec3(0,1,0), -30);
-    tri1->rotateSelf(vec3(0,0,1), 180);
+    tri1->rotateSelf(vec3(0,0,1), 170);
     // tri1->rotateSelf(vec3(0,1,0), -30);
     tri1->translateSelf(vec3(0,500,-50));
 
 
-    plane1->setMaterial(__REFLECTIVE__);
+    // plane1->setMaterial(__REFLECTIVE__);
     tri1->setMaterial(__REFLECTIVE__);
-    // sphere2->setMaterial(__REFLECTIVE__);
+    sphere2->setMaterial(__REFLECTIVE__);
     // sphere->setMaterial(__REFLECTIVE__);
     cone1->setMaterial(__REFLECTIVE__);
     cylinder1->setMaterial(__REFLECTIVE__);
@@ -80,14 +85,10 @@ int main() {
     
 
     plane1->setTexture(checkerPlaneTexture);
-    // sphere1->texture()
-    // sphere2->texture();
-    // cone1->texture();
-    // cylinder1->texture();
 
-    camera->translateSelf(vec3(0,0,-50));
-    camera->rotate(vec3(0,440,0), camera->right, -45);
-    camera->rotate(vec3(0,440,0), vec3(0,0,1), 45);
+    // camera->translateSelf(vec3(0,0,-50));
+    // camera->rotate(vec3(0,440,0), camera->right, -45);
+    // camera->rotate(vec3(0,440,0), vec3(0,0,1), 45);
 
     scene.render(10, 1, "image.bmp");
     clock_t end = clock();
--- surface.cpp
+++ surface.cpp
@@ -5,19 +5,19 @@
 Sphere::Sphere(vec3 center, double radius): 
     _radius(radius) {
 
-    this->_center = center;
+    _center = center;
 
-    this->_up = vec3(0,0,1);
-    this->_right = vec3(1,0,0);
-    this->_cross = vec3(0,1,0);
+    _up = vec3(0,0,1);
+    _right = vec3(1,0,0);
+    _cross = vec3(0,1,0);
 }
 
 bool Sphere::hit(Ray &ray) {
 
     double a = dot(ray.direction, ray.direction);
-    double b = 2*dot(ray.direction, ray.origin - this->_center);
-    double c = dot(ray.origin, ray.origin) + dot(this->_center, this->_center)
-                - 2*(dot(ray.origin, this->_center)) - (this->_radius)*(this->_radius);
+    double b = 2*dot(ray.direction, ray.origin - _center);
+    double c = dot(ray.origin, ray.origin) + dot(_center, _center)
+                - 2*(dot(ray.origin, _center)) - _radius*_radius;
     double k = b*b - 4*a*c;
     if (k > 0) {
         k = sqrt(k);
@@ -35,10 +35,10 @@ bool Sphere::hit(Ray &ray) {
 }
 
 void Sphere::calculateValues(Ray &ray) {
-    this->_normal = (ray.at(this->_t)-this->_center).unit();
-    this->_outer_surface_normal = this->_normal;
-    if (dot(ray.direction, this->_normal) > 0) { this->_normal = -1*this->_normal; }
-    this->_color = this->texture.getColor(ray.at(this->_t), this->_center, this->_up, this->_right, this->_cross);
+    _normal = (ray.at(_t)-_center).unit();
+    _outer_surface_normal = _normal;
+    if (dot(ray.direction, _normal) > 0) { _normal = -1*_normal; }
+    _color = this->texture.getColor(ray.at(_t), _center, _up, _right, _cross);
 }
 
 
@@ -46,33 +46,33 @@ void Sphere::calculateValues(Ray &ray) {
 Cylinder::Cylinder(vec3 center, double radius, double height): 
     _radius(radius), _height(height) {
 
-    this->_center = center;
+    _center = center;
 
-    this->_up = vec3(0,0,1);
-    this->_right = vec3(1,0,0);
-    this->_cross = vec3(0,1,0);
+    _up = vec3(0,0,1);
+    _right = vec3(1,0,0);
+    _cross = vec3(0,1,0);
 }
 
 bool Cylinder::hit(Ray &ray) {
 
-    double da = dot(ray.direction, this->_up);
-    double qa = dot(ray.origin, this->_up);
-    double ca = dot(this->_center, this->_up);
+    double da = dot(ray.direction, _up);
+    double qa = dot(ray.origin, _up);
+    double ca = dot(_center, _up);
     double a = dot(ray.direction, ray.direction) - da*da;
-    double b = 2*dot(ray.direction, ray.origin-this->_center) - 2*da*(qa-ca);
-    double c = dot(ray.origin, ray.origin)-qa*qa+dot(this->_center, this->_center)
-        -ca*ca-2*dot(ray.origin, this->_center)+2*(qa*ca)-(this->_radius*this->_radius);
+    double b = 2*dot(ray.direction, ray.origin-_center) - 2*da*(qa-ca);
+    double c = dot(ray.origin, ray.origin)-qa*qa+dot(_center, _center)
+        -ca*ca-2*dot(ray.origin, _center)+2*(qa*ca)-_radius*_radius;
 
     double k = b*b - 4*a*c;
     if (k > 0) {
         k = sqrt(k);
         double t = (-b-k)/(2*a);
-        double height = dot(ray.at(t)-this->_center, this->_up);
+        double height = dot(ray.at(t)-_center, _up);
         if (t < 0 || height > this->_height || height < 0) {
             t = (-b+k)/(2*a);
             if (t < 0) { return false; }
         }
-        height = dot(ray.at(t)-this->_center, this->_up);
+        height = dot(ray.at(t)-_center, _up);
         if (height > this->_height || height < 0) { return false; }
         this->_t = t;
         return true;
@@ -81,11 +81,11 @@ bool Cylinder::hit(Ray &ray) {
 }
 
 void Cylinder::calculateValues(Ray &ray) {
-    vec3 r = ray.at(this->_t) - this->_center;
-    this->_normal = (r - dot(r, this->_up)*this->_up).unit();
-    this->_outer_surface_normal = this->_normal;
-    if (dot(ray.direction, this->_normal) > 0) { this->_normal = -1*this->_normal; }
-    this->_color = this->texture.getColor(ray.at(this->_t), this->_center, this->_up, this->_right, this->_cross);
+    vec3 r = ray.at(_t) - _center;
+    _normal = (r - dot(r, _up)*_up).unit();
+    _outer_surface_normal = _normal;
+    if (dot(ray.direction, _normal) > 0) { _normal = -1*_normal; }
+    _color = this->texture.getColor(ray.at(_t), _center, _up, _right, _cross);
 }
 
 
@@ -94,36 +94,34 @@ void Cylinder::calculateValues(Ray &ray) {
 Cone::Cone(vec3 center, double angle, double height): 
     _angle(angle*M_PI/180), _height(height), _cos_angle(cos(angle*M_PI/180)), _tan_angle(tan(angle*M_PI/180)) {
 
-    this->_t = 0;
-    this->_normal = vec3();
-    this->_center = center;
+    _center = center;
 
-    this->_up = vec3(0,0,1);
-    this->_right = vec3(1,0,0);
-    this->_cross = vec3(0,1,0);
+    _up = vec3(0,0,1);
+    _right = vec3(1,0,0);
+    _cross = vec3(0,1,0);
 }
 
 bool Cone::hit(Ray &ray) {
 
-    double da = dot(ray.direction, this->_up);
-    double qa = dot(ray.origin, this->_up);
-    double ca = dot(this->_center, this->_up);
-    double cost = this->_cos_angle;
+    double da = dot(ray.direction, _up);
+    double qa = dot(ray.origin, _up);
+    double ca = dot(_center, _up);
+    double cost = _cos_angle;
     double a = dot(ray.direction, ray.direction)*cost*cost - da*da;
-    double b = 2*dot(ray.direction, ray.origin-this->_center)*cost*cost - 2*da*(qa-ca);
-    double c = (dot(ray.origin, ray.origin)+dot(this->_center, this->_center)
-        -2*dot(ray.origin, this->_center))*cost*cost - qa*qa - ca*ca + 2*qa*ca;
+    double b = 2*dot(ray.direction, ray.origin-_center)*cost*cost - 2*da*(qa-ca);
+    double c = (dot(ray.origin, ray.origin)+dot(_center, _center)
+        -2*dot(ray.origin, _center))*cost*cost - qa*qa - ca*ca + 2*qa*ca;
 
     double k = b*b-4*a*c;
     if (k > 0) {
         k = sqrt(k);
         double t = (-b-k)/(2*a);
-        double height = dot(ray.at(t)-this->_center, this->_up);
+        double height = dot(ray.at(t)-_center, _up);
         if (t < 0 || height > this->_height || height < 0) {
             t = (-b+k)/(2*a);
             if (t < 0) { return false; }
         }
-        height = dot(ray.at(t)-this->_center, this->_up);
+        height = dot(ray.at(t)-_center, _up);
         if (height > this->_height || height < 0) { return false; }
         this->_t = t;
         return true;
@@ -132,41 +130,41 @@ bool Cone::hit(Ray &ray) {
 }
 
 void Cone::calculateValues(Ray &ray) {
-    vec3 r = ray.at(this->_t) - this->_center;
-    vec3 s = (r - dot(r, this->_up)*this->_up).unit();
-    this->_normal = (s - (this->_tan_angle*this->_up)).unit();
-    this->_outer_surface_normal = this->_normal;
-    if (dot(ray.direction, this->_normal) > 0) { this->_normal = -1*this->_normal; }
-    this->_color = this->texture.getColor(ray.at(this->_t), this->_center, this->_up, this->_right, this->_cross);
+    vec3 r = ray.at(_t) - _center;
+    vec3 s = (r - dot(r, _up)*_up).unit();
+    _normal = (s - (_tan_angle*_up)).unit();
+    _outer_surface_normal = _normal;
+    if (dot(ray.direction, _normal) > 0) { _normal = -1*_normal; }
+    _color = this->texture.getColor(ray.at(_t), _center, _up, _right, _cross);
 }
 
 
 
 Plane::Plane(vec3 center, vec3 normal) {
-    this->_normal = normal.unit();
-    this->_center = center;
+    _normal = normal.unit();
+    _center = center;
 
     // ensure that basis vectors are orthogonal
-    this->_up = this->_normal;
-    this->_right = vec3(1,0,0);
-    if (cross(this->_up, this->_right).mag() < 1e-10) {
-        this->_normal = vec3(1,0,0);
-        this->_up = vec3(1,0,0);
-        this->_right = vec3(0,0,-1);
-        this->_cross = vec3(0,1,0);
+    _up = _normal;
+    _right = vec3(1,0,0);
+    if (cross(_up, _right).mag() < 1e-10) {
+        _normal = vec3(1,0,0);
+        _up = vec3(1,0,0);
+        _right = vec3(0,0,-1);
+        _cross = vec3(0,1,0);
     }
     else {
-        this->_cross = cross(this->_up, this->_right).unit();
-        this->_right = cross(this->_cross, this->_up);
+        _cross = cross(_up, _right).unit();
+        _right = cross(_cross, _up);
     }
 
-    this->_outer_surface_normal = this->_normal;
+    _outer_surface_normal = _normal;
 }
 
 bool Plane::hit(Ray &ray) {
-    double denom = dot(this->_up, ray.direction);
+    double denom = dot(_up, ray.direction);
     if (denom == 0) { return false; }
-    double t = dot(this->_up, this->_center-ray.origin)/denom;
+    double t = dot(_up, _center-ray.origin)/denom;
     if (t > 1e-10) {
         this->_t = t;
         return true;
@@ -175,16 +173,16 @@ bool Plane::hit(Ray &ray) {
 }
 
 void Plane::calculateValues(Ray &ray) {
-    if (dot(ray.direction, this->_normal) > 0) { this->_normal = -1*this->_normal; }
-    this->_color = this->texture.getColor(ray.at(this->_t), this->_center, this->_up, this->_right, this->_cross);
+    if (dot(ray.direction, _normal) > 0) { _normal = -1*_normal; }
+    _color = this->texture.getColor(ray.at(_t), _center, _up, _right, _cross);
 }
 
 void Plane::rotateSelf(const vec3 &axis, double angle) {
-    this->_normal.rotate(axis, angle);
-    this->_outer_surface_normal.rotate(axis, angle);
-    this->_up.rotate(axis, angle);
-    this->_right.rotate(axis, angle);
-    this->_cross.rotate(axis, angle);
+    _normal.rotate(axis, angle);
+    _outer_surface_normal.rotate(axis, angle);
+    _up.rotate(axis, angle);
+    _right.rotate(axis, angle);
+    _cross.rotate(axis, angle);
 }
 
 
@@ -192,28 +190,27 @@ void Plane::rotateSelf(const vec3 &axis, double angle) {
 Triangle::Triangle(vec3 v0, vec3 v1, vec3 v2):
     _v0(v0), _v1(v1), _v2(v2), _normal_unnormalized(cross(v1-v0, v2-v0)), _area(_normal_unnormalized.mag()/2), _u(0), _v(0), _w(0) {
 
-    this->_t = 0;
-    this->_normal = cross(v1-v0, v2-v0).unit();
-    this->_center = v0;
+    _normal = cross(v1-v0, v2-v0).unit();
+    _center = v0;
 
-    this->_up = _normal;
-    this->_right = (v1-v0).unit();
-    this->_cross = cross(this->_up, this->_right).unit();
+    _up = _normal;
+    _right = (v1-v0).unit();
+    _cross = cross(_up, _right).unit();
 
-    this->_outer_surface_normal = this->_up;
+    _outer_surface_normal = _up;
 }
 
 bool Triangle::hit(Ray &ray) {
 
-    double denom = dot(this->_up, ray.direction);
+    double denom = dot(_up, ray.direction);
     if (denom == 0) { return false; }
-    double t = dot(this->_up, this->_center-ray.origin)/denom;
+    double t = dot(_up, _center-ray.origin)/denom;
     if (t > 1e-10) {
-        this->_t = t;
-        vec3 point = ray.at(t) - this->_v0;
-        double u = dot(cross(this->_v1 - this->_v0, point), this->_up)/2/this->_area;
+        _t = t;
+        vec3 point = ray.at(t) - _v0;
+        double u = dot(cross(_v1 - _v0, point), _up)/2/_area;
         if (u > 0) { 
-            double v = dot(cross(point, this->_v2 - this->_v0), this->_up)/2/this->_area;
+            double v = dot(cross(point, _v2 - _v0), _up)/2/_area;
             if (v > 0 && 1-u-v > 0) {
                 this->_u = u;
                 this->_v = v;
@@ -226,42 +223,42 @@ bool Triangle::hit(Ray &ray) {
 }
 
 void Triangle::calculateValues(Ray &ray) {
-    if (dot(ray.direction, this->_normal) > 0) {
-        this->_normal = -1*this->_normal;
-        this->_normal_unnormalized = -1*this->_normal_unnormalized;
+    if (dot(ray.direction, _normal) > 0) {
+        _normal = -1*_normal;
+        _normal_unnormalized = -1*_normal_unnormalized;
     }
-    this->_color = this->texture.getColor(ray.at(this->_t), this->_center, this->_up, this->_right, this->_cross);
+    _color = this->texture.getColor(ray.at(_t), _center, _up, _right, _cross);
 }
 
 void Triangle::translateSelf(const vec3 &translation) {
-    this->_center.translate(translation);
-    this->_v0.translate(translation);
-    this->_v1.translate(translation);
-    this->_v2.translate(translation);
+    _center.translate(translation);
+    _v0.translate(translation);
+    _v1.translate(translation);
+    _v2.translate(translation);
 }
 
 void Triangle::rotateSelf(const vec3 &axis, double angle) {
-    this->_normal.rotate(axis, angle);
-    this->_outer_surface_normal.rotate(axis, angle);
-    this->_up.rotate(axis, angle);
-    this->_right.rotate(axis, angle);
-    this->_cross.rotate(axis, angle);
+    _normal.rotate(axis, angle);
+    _outer_surface_normal.rotate(axis, angle);
+    _up.rotate(axis, angle);
+    _right.rotate(axis, angle);
+    _cross.rotate(axis, angle);
 
-    this->_normal_unnormalized.rotate(axis, angle);
-    this->_v1.rotate(this->_v0, axis, angle);
-    this->_v2.rotate(this->_v0, axis, angle);
+    _normal_unnormalized.rotate(axis, angle);
+    _v1.rotate(_v0, axis, angle);
+    _v2.rotate(_v0, axis, angle);
 }
 
 void Triangle::rotate(const vec3 &point, const vec3 &axis, double angle) {
-    this->_normal.rotate(axis, angle);
-    this->_outer_surface_normal.rotate(axis, angle);
-    this->_up.rotate(axis, angle);
-    this->_right.rotate(axis, angle);
-    this->_cross.rotate(axis, angle);
+    _normal.rotate(axis, angle);
+    _outer_surface_normal.rotate(axis, angle);
+    _up.rotate(axis, angle);
+    _right.rotate(axis, angle);
+    _cross.rotate(axis, angle);
 
-    this->_normal_unnormalized.rotate(axis, angle);
-    this->_center.rotate(point, axis, angle);
-    this->_v0.rotate(point, axis, angle);
-    this->_v1.rotate(point, axis, angle);
-    this->_v2.rotate(point, axis, angle);
+    _normal_unnormalized.rotate(axis, angle);
+    _center.rotate(point, axis, angle);
+    _v0.rotate(point, axis, angle);
+    _v1.rotate(point, axis, angle);
+    _v2.rotate(point, axis, angle);
 }
